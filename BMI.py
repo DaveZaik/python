67371bd60cd577f0a1b4fe6d735cdd731eb71ec9
davezaik <thaikudavid0@gmail.com>
2023-10-02 14:53:49 -0700

    done

--- BMI.py
+++ BMI.py
@@ -1,7 +1,7 @@
 # list
 # tuple
-height=0.9
-weight=72
+height=float(input("Enter Height in meters: "))
+weight=int(input("Enter weight in kgs: "))
 BMI=weight/height**2
 print(BMI)
 if BMI >=18.5 and BMI<=22.9:
@@ -14,7 +14,7 @@ elif BMI >=23 and BMI<=24.9:
  print("Overweight")
 
 elif BMI >=25 and BMI<=29.9:
- print(" Pre-Obese")
+ print(" Pre-Obese") 
 
 elif BMI >=30 and BMI<=34.9:
  print("Obese class I")
@@ -24,3 +24,6 @@ elif BMI >=35 and BMI<=39.9:
 
 elif BMI >=40.0:
  print("Obese class III")
+
+else:
+ print ("Invalid Input")
